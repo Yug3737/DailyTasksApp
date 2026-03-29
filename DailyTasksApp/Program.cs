d5ab91f0ab9d57a99dde01e470f7f784e7ba2eef
Yug3737 <yugpatel8767@gmail.com>
2026-03-29 19:18:30 -0400

    database connection debugging

--- DailyTasksApp/Program.cs
+++ DailyTasksApp/Program.cs
@@ -6,19 +6,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+var connectionString = builder.Configuration.GetConnectionString("ProdConnection")
                        ?? throw new InvalidOperationException("Connection string" +
                                                               "'DefaultConnection' not found.");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer((connectionString)));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.CanConnect();
 }
+catch (Exception ex)
+{
+    Console.WriteLine($"DB ERROR: {ex.Message}");
+    throw;
+}
+
+// Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
 app.UseHttpsRedirection();
 app.UseRouting();
