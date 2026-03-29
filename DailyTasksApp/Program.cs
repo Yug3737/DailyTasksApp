adc3076088d55c1330d508d251d6a8374127ee9f
Yug3737 <yugpatel8767@gmail.com>
2026-03-29 19:26:30 -0400

    debugging over, app is live

--- DailyTasksApp/Program.cs
+++ DailyTasksApp/Program.cs
@@ -12,25 +12,25 @@ var connectionString = builder.Configuration.GetConnectionString("ProdConnection
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer((connectionString)));
 var app = builder.Build();
 
-try
-{
-    using var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.CanConnect();
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"DB ERROR: {ex.Message}");
-    throw;
-}
+// try
+// {
+//     using var scope = app.Services.CreateScope();
+//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     db.Database.CanConnect();
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"DB ERROR: {ex.Message}");
+//     throw;
+// }
 
 // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // app.UseHsts(); // UseHsts likely causes problem on free tier of azurewebsites.net, hence have removed it.
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
